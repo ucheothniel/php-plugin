fe88954f8b55d41fe871b778236f9c294bdbe0b5
ucheothniel <59846495+ucheothniel@users.noreply.github.com>
2021-01-29 11:23:58 -0500

    Add files via upload

---
+++ my-wp-project/my-php-project.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: my-php-project
+Plugin URI: 
+Description: Plugin to pull and display information at and specified endpoint.
+Version: 1.0
+Author: Uche Othniel Osakwe
+Author URI: 
+License: GPLv2 or later
+Text Domain: 
+*/
